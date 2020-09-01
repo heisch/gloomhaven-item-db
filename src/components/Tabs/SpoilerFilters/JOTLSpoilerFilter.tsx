c60df110354d787e9142e4f409a6113de50d8b45
Jim Solomon <cgbspender1971@hotmail.com>
2020-09-01 14:28:24 -0400

    Added Better item filtering for jotl

--- src/components/Tabs/SpoilerFilters/JOTLSpoilerFilter.tsx
+++ src/components/Tabs/SpoilerFilters/JOTLSpoilerFilter.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import SpoilerFilterItemList from './SpoilerFilterItemList';
-
+import { Form } from 'semantic-ui-react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../../../State/Reducer';
+import { storeScenarioCompleted } from '../../../State/SpoilerFilter';
 
 const JOTLSpoilerFilter = () => {
+    const scenariosOfImportance = [2, 9, 15];
+    const { scenarioCompleted } = useSelector<RootState>( state => state.spoilerFilter) as RootState['spoilerFilter'];
+    const dispatch = useDispatch();
+
+    const toggleScenarioCompleted = (key: number) => {
+        if (scenarioCompleted.includes(key)) {
+            scenarioCompleted.splice(scenarioCompleted.indexOf(key), 1);
+        } else {
+            scenarioCompleted.push(key)
+        }
+        dispatch(storeScenarioCompleted(scenarioCompleted));
+    }
+
     return (
-        <SpoilerFilterItemList start={14} end={36} title="Items"/>
+        <>
+            <Form.Group inline className={'inline-break'}>
+                <label>Scenarios Completed:</label>
+                {scenariosOfImportance.map( (id) => {
+                    return <Form.Checkbox key={id} label={'#' + (id + '').padStart(3, '0')}
+                        checked={scenarioCompleted.includes(id)}
+                        onChange={() => toggleScenarioCompleted(id)}/> 
+                })}
+            </Form.Group>
+            <SpoilerFilterItemList ranges={[{start:14, end:14}, {start:27, end:36}]} title="Items"/>
+        </>
     )
 }
 
--- src/components/Tabs/SpoilerFilters/SpoilerFilterItemList.tsx
+++ src/components/Tabs/SpoilerFilters/SpoilerFilterItemList.tsx
@@ -2,21 +2,48 @@ import React from 'react'
 import { Form } from 'semantic-ui-react';
 import FilterCheckbox from './FilterCheckbox';
 
-type Props = {
-    start: number;
+type Range = {
+    start:number;
     end: number;
+}
+
+type Props = {
+    start?:number;
+    end?:number;
+    ranges?: Array<Range>;
     title: string;
 }
 
 const SpoilerFilterItemList = (props:Props) => {
-    const {start, end, title} = props;
+    const {start, end, ranges, title} = props;
 
-    const checkBoxes = [];
-    for (let i = start; i <= end; i++) {
-        checkBoxes.push(<FilterCheckbox key={`filter${i}`} id={i}/>);
+    if (start && end && ranges)
+    {
+        throw Error("Cannot use list with both start/end and ranges")
     }
 
-    if (start >= end) {
+    if (!start && !end && !ranges)
+    {
+        throw Error("You must specify a range")
+
+    }
+
+    let rangesToShow = ranges || [];
+    if (rangesToShow.length === 0 && start && end)
+    {
+        rangesToShow.push({start, end});
+    }
+
+    const checkBoxes:Array<any> = [];
+    rangesToShow.forEach(range=> {
+       const { start, end} = range;
+        for (let i = start; i <= end; i++) {
+            checkBoxes.push(<FilterCheckbox key={`filter${i}`} id={i}/>);
+        }
+            
+    });
+    if (checkBoxes.length === 0)
+    {
         return null;
     }
 
