246a27566f788ca8f06028399f283cc69b4dac10
Jim Solomon <cgbspender1971@hotmail.com>
2020-10-25 22:27:23 -0400

    Added Dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import { Container } from 'semantic-ui-react'
+import { Container, DropdownProps, Form } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 import dbApp from "./State/Reducer";
 import MainView from './components/Tabs/MainView/MainView';
 import GameProvider from './components/Game/GameProvider'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import { GameType } from './games';
-import Navigation from './components/Navigation';
+import { gameDataTypes, GameType } from './games';
 
 export const store = createStore(dbApp,  (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__());
 
+type GameSelectorProps = {
+    onChange:(obj:any,e:DropdownProps) => void;
+    defaultGameType:GameType;
+}
+
+const GameSelector = (props:GameSelectorProps) => {
+    const {onChange, defaultGameType} = props;
+    const options:any[] = [];
+    Object.values(GameType).forEach( (gameType) =>{
+        const gameData = gameDataTypes[gameType as GameType];
+        options.push({text:gameData.name, value:gameType});
+    } )
+
+    return <>    
+        <Form.Select 
+            value={defaultGameType}
+            options={options}
+            onChange={onChange}/>
+        </>
+}
+
+
 const App = () => {
+    const [gameType, setGameType] = useState<GameType>(GameType.Gloomhaven);
+
+    const onGameTypeChanged = (obj:any, e:DropdownProps):void => {
+        setGameType(e.value as GameType);
+    }
+
     return (
         <Container>
+            <GameSelector defaultGameType={gameType} onChange={onGameTypeChanged}/>
             <Provider store={store}>
-                <Router>
-                    <Navigation/>
-                    <Switch>
-                        <Route exact path="/">
-                            <GameProvider gameType={GameType.Gloomhaven}>
-                                <MainView/>
-                            </GameProvider>
-                        </Route>                    
-                        <Route exact path="/jotl">
-                        <GameProvider gameType={GameType.JawsOfTheLion}>
-                            <MainView/>
-                        </GameProvider>
-                        </Route>                    
-                    </Switch>
-                </Router>
+                <GameProvider gameType={gameType}>
+                    <MainView/>
+                </GameProvider>
             </Provider>
         </Container>
     );
