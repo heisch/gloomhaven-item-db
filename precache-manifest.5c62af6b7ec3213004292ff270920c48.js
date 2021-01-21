74692b089b811b6b792a111934944bce5c580c39
Jim Solomon <jsolomon@vistaprint.com>
2021-01-20 22:26:20 -0500

    Updates

--- precache-manifest.5c62af6b7ec3213004292ff270920c48.js
+++ precache-manifest.5c62af6b7ec3213004292ff270920c48.js
@@ -184,32 +184,24 @@ self.__precacheManifest = [
     "url": "/gloomhaven-item-db/static/media/flags.9c74e172.png"
   },
   {
-    "revision": "0ab54153eeeca0ce03978cc463b257f7",
-    "url": "/gloomhaven-item-db/static/media/icons.0ab54153.woff2"
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/gloomhaven-item-db/static/media/icons.faff9214.woff"
   },
   {
     "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
     "url": "/gloomhaven-item-db/static/media/icons.8e3c7f55.eot"
   },
   {
-    "revision": "faff92145777a3cbaf8e7367b4807987",
-    "url": "/gloomhaven-item-db/static/media/icons.faff9214.woff"
-  },
-  {
-    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
-    "url": "/gloomhaven-item-db/static/media/icons.b87b9ba5.ttf"
-  },
-  {
-    "revision": "cd6c777f1945164224dee082abaea03a",
-    "url": "/gloomhaven-item-db/static/media/outline-icons.cd6c777f.woff2"
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/gloomhaven-item-db/static/media/icons.0ab54153.woff2"
   },
   {
     "revision": "701ae6abd4719e9c2ada3535a497b341",
     "url": "/gloomhaven-item-db/static/media/outline-icons.701ae6ab.eot"
   },
   {
-    "revision": "962a1bf31c081691065fe333d9fa8105",
-    "url": "/gloomhaven-item-db/static/media/icons.962a1bf3.svg"
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/gloomhaven-item-db/static/media/icons.b87b9ba5.ttf"
   },
   {
     "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
@@ -220,12 +212,24 @@ self.__precacheManifest = [
     "url": "/gloomhaven-item-db/static/media/outline-icons.ad97afd3.ttf"
   },
   {
-    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
-    "url": "/gloomhaven-item-db/static/media/outline-icons.82f60bd0.svg"
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/gloomhaven-item-db/static/media/icons.962a1bf3.svg"
   },
   {
     "revision": "e8c322de9658cbeb8a774b6624167c2c",
     "url": "/gloomhaven-item-db/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/gloomhaven-item-db/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/gloomhaven-item-db/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/gloomhaven-item-db/static/media/brand-icons.c5ebe0b3.ttf"
   },
   {
     "revision": "a046592bac8f2fd96e994733faf3858c",
@@ -234,10 +238,6 @@ self.__precacheManifest = [
   {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
     "url": "/gloomhaven-item-db/static/media/brand-icons.13db00b7.eot"
-  },
-  {
-    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
-    "url": "/gloomhaven-item-db/static/media/brand-icons.c5ebe0b3.ttf"
   },
   {
     "revision": "a1a749e89f578a49306ec2b055c073da",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gloomhaven-item-db/precache-manifest.9a8fb7b224cb5dd7fc56be55abf9ed1e.js"
+  "/gloomhaven-item-db/precache-manifest.5c62af6b7ec3213004292ff270920c48.js"
 );
 
 workbox.clientsClaim();
