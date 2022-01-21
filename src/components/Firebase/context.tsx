e416d4bed16428aa8aa58625bf54edf53c927200
Jim Solomon <jsolomon@vistaprint.com>
2022-01-20 23:48:02 -0500

    Moved Import to Firebase Class

--- src/components/Firebase/context.tsx
+++ src/components/Firebase/context.tsx
@@ -1,10 +1,12 @@
 import React, { useState, useContext, createContext, useEffect, FC, useMemo } from 'react';
 import Firebase from './firebase';
+import qs from "qs";
 
 type Data = {
     firebase: Firebase | null | undefined;
     authUser: firebase.default.User | null | undefined;
     remoteData: string | undefined;
+    importData: string | undefined;
 }
 
 const FirebaseContext = createContext<Data | undefined>(undefined);
@@ -23,6 +25,7 @@ const FirebaseProvider: FC = ({ children }) => {
     const [firebase, setFirebase] = useState<Firebase>();
     const [authUser, setAuthUser] = useState<firebase.default.User | null>();
     const [remoteData, setRemoteData] = useState<string|undefined>();
+    const [importData, setImportData] = useState<string|undefined>();
     useEffect( () => {
         setFirebase(new Firebase());
     }, [])
@@ -46,6 +49,30 @@ const FirebaseProvider: FC = ({ children }) => {
     }, [firebase])
 
     useEffect(() => {
+        if (!firebase) {
+          return;
+        }
+
+        const urlParams = qs.parse(window.location.search.substr(1));
+        const importUserId = urlParams["importFrom"] as string;
+        if (!importUserId) { 
+            return;
+        }
+    
+        firebase
+            .spoilerFilter(importUserId).on("value", (snapshot) => {
+                if (snapshot.val())
+                {
+                setImportData(snapshot.val()["configHash"]);
+                }
+                return;
+            },
+            (error: any)=>  {
+            console.log(error)
+            })
+      }, [firebase])
+
+    useEffect(() => {
         if (!firebase || !authUser) {
             return;
         }
@@ -53,8 +80,8 @@ const FirebaseProvider: FC = ({ children }) => {
     },[firebase, authUser]);
 
     const value = useMemo(() => (
-        {firebase, authUser, remoteData}
-    ), [firebase, authUser, remoteData]);
+        {firebase, authUser, remoteData, importData}
+    ), [firebase, authUser, remoteData, importData]);
 
     return <Provider value={value}>{children}</Provider>
 }
--- src/components/Tabs/MainView/ImportData.tsx
+++ src/components/Tabs/MainView/ImportData.tsx
@@ -1,51 +1,21 @@
 import React, { useEffect, useState } from "react";
-import qs from "qs";
 import { Button, Header, Icon, Modal } from "semantic-ui-react";
 import { useFilterOptions } from "../../Providers/FilterOptionsProvider";
 import { useFirebase } from "../../Firebase";
 
 const ImportData = () => {
     const { loadFromHash, getImportHash, getShareHash } = useFilterOptions();
-    const { firebase } = useFirebase();
+    const { importData } = useFirebase();
     const qsImportHash = getImportHash();
     const [importHash, setImportHash] = useState(qsImportHash);
     const [isOpen, setIsOpen] = useState(importHash && importHash.length > 0 || false);
     const currentHash = getShareHash(localStorage.getItem("lockSpoilerPanel") === "true");
 
     useEffect( () => {
-      if (qsImportHash) {
-        setImportHash(qsImportHash);
+      if (qsImportHash || importData) {
+        setImportHash(qsImportHash || importData);
       }
-
-    }, [qsImportHash])
-
-    useEffect(() => {
-      if (!firebase) {
-        return;
-      }
-      
-      if (importHash || qsImportHash) {
-        return;
-      }
-      
-        const urlParams = qs.parse(window.location.search.substr(1));
-        const importUserId = urlParams["importFrom"] as string;
-        if (!importUserId) { 
-            return;
-        }
-  
-        firebase
-            .spoilerFilter(importUserId).on("value", (snapshot) => {
-                if (snapshot.val())
-                {
-                  setImportHash(snapshot.val()["configHash"]);
-                }
-                return;
-            },
-            (error: any)=>  {
-              console.log(error)
-            })
-    }, [firebase])
+    }, [qsImportHash, importData])
 
     useEffect(() => {
       if (importHash && importHash !== currentHash) {
